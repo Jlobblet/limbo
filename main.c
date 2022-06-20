73d661a746e54e12150888be320955b55473bd5d
John Blundell <jlobblet+github@jlobblet.co.uk>
2022-06-20 01:57:34 +0100

    Lexer test

--- main.c
+++ main.c
@@ -1,26 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "lexer.h"
 
+void print_token(Token* token) {
+    char *kind;
+    char buffer[100] = { 0 };
+    switch (token->kind) {
+        case TOKEN_IDENTIFIER:
+            strncpy(buffer, token->location, token->length);
+            buffer[token->length] = '\0';
+            kind = "IDENTIFIER";
+            break;
+        case TOKEN_PUNCTUATOR:
+            strncpy(buffer, token->location, token->length);
+            buffer[token->length] = '\0';
+            kind = "PUNCTUATOR";
+            break;
+        case TOKEN_KEYWORD:
+            strncpy(buffer, token->location, token->length);
+            buffer[token->length] = '\0';
+            kind = "KEYWORD";
+            break;
+        case TOKEN_STRING:
+            strncpy(buffer, token->string_value, token->length);
+            buffer[token->length] = '\0';
+            kind = "STRING";
+            break;
+        case TOKEN_INTEGRAL:
+            sprintf(buffer, "%ld", token->int_value);
+            kind = "INTEGRAL";
+            break;
+        case TOKEN_REAL:
+            sprintf(buffer, "%f", token->real_value);
+            kind = "REAL";
+            break;
+        case TOKEN_EOF:
+            kind = "EOF";
+            break;
+    }
+
+    printf("%s: %s\n", kind, buffer);
+}
+
 int main() {
-    char *num = "621";
+    char *program =
+            "implement Command;"
+            "include \"sys.m\";"
+            "include \"draw.m\";"
+            "sys:    Sys;"
+            "Command: module {"
+            "    init: fn (ctxt: ref Draw->Context, argv: list of string);"
+            "};"
+            "# The canonical \"Hello world\" program, enhanced"
+            "init(ctxt: ref Draw->Context, argv: list of string)"
+            "{"
+            "    sys = load Sys Sys->PATH;"
+            "    sys->print(\"hello world!\\n\");"
+            "    for (; argv!=nil; argv = tl argv)"
+            "        sys->print(\"%s \", hd argv);"
+            "    sys->print(\"\\n\");"
+            "}";
     SourceFile file = {
-        .name = "test",
-        .contents = num,
+        .name = "test.m",
+        .contents = program,
         .file_number = 1
     };
-    LexerContext context = {
-        .source_file = &file,
-        .at_beginning_of_line = true,
-        .follows_space = false,
-    };
-    char *position;
-    Token *token = read_number_literal(&context, num, &position);
-    printf("%ld\n", token->int_value);
+    Token *head = lex(&file);
 
-    num = "420.69";
-    token = read_number_literal(&context, num, &position);
-    printf("%f\n", token->real_value);
+    while (head != NULL) {
+        print_token(head);
+        head = head->next;
+    }
 
     return EXIT_SUCCESS;
 }
